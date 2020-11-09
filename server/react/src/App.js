9fcbc09023082deebcf14d7da71042cb36c5adf0
Timothy Poon <timothy.poon520@gmail.com>
2020-11-08 23:11:29 -0800

    Add download for extension

--- server/react/src/App.js
+++ server/react/src/App.js
@@ -3,12 +3,16 @@ import { Route, Switch } from "react-router-dom";
 import Header from './components/Header';
 import './App.scss';
 import Initiative from './components/Initiative';
+import Extension from './components/Extension';
 
 const App = () => {
     return (
         <div className="App">
             <Header />
             <Switch>
+                <Route path="/extension">
+                    <Extension />
+                </Route>
                 <Route path="/">
                     <div className="App-container"><Initiative /></div>
                 </Route>
